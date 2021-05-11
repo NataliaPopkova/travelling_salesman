49951b48b5e00b229e3aea3186502e6784e5eb6d
NataliaP <2000_natalia@mail.ru>
2021-05-11 16:17:04 +0300

    add: файлы для графики // исправила ошибку в map.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "map.h"
 
-int main(int argc, char **argv)
-{
-
+int main(int argc, char** argv) {
     std::srand(std::time(nullptr));
     Map map(100);
     map.InitializeCities();
@@ -10,9 +8,9 @@ int main(int argc, char **argv)
     std::ofstream file("Cities.txt");
     if (!file.is_open())
         std::cout << "Cities: File wasn't open!\n";
-    for (int i = 0; i < map.GetWidth(); ++i)
-    {
-        file << map.GetCities()[i].first << "    " << map.GetCities()[i].second << std::endl;
+    for (int i = 0; i < map.GetWidth(); ++i) {
+        file << map.GetCities()[i].first << "    " << map.GetCities()[i].second
+             << std::endl;
     }
 
     map.GetSeq() = SimulatedAnnealing(map, 10, 0.00001);
@@ -20,15 +18,16 @@ int main(int argc, char **argv)
     std::ofstream file1("Seq.txt");
     if (!file1.is_open())
         std::cout << "Seq: File wasn't open!\n";
-    for (int i = 0; i < map.GetWidth(); ++i)
-    {
+    for (int i = 0; i < map.GetWidth(); ++i) {
         file1 << map.GetSeq()[i] << std::endl;
     }
     file1 << map.GetSeq()[0] << std::endl;
 
-    for (int i = 0; i < map.GetHeight(); ++i)
-    {
-        std::cout << map.GetSeq()[i] << std::endl;
-    }
+    // for (int i = 0; i < map.GetHeight(); ++i)
+    // {
+    //     std::cout << map.GetSeq()[i] << std::endl;
+    // }
+
+    std::cout << "done!";
     return 0;
 }
--- map.cpp
+++ map.cpp
@@ -1,93 +1,89 @@
 #include "map.h"
 
-Map::Map(size_t size) : size(size),
-                        Cities(size, std::make_pair(0, 0)),
-                        Seq(size)
-{
+Map::Map(size_t size)
+        : size(size), Cities(size, std::make_pair(0, 0)), Seq(size) {}
+
+size_t Map::GetWidth() const {
+    return size;
+}  //возвращает количество столбцов
+size_t Map::GetHeight() const {
+    return size;
+}  // возвращает количество строк
+
+std::vector<std::pair<double, double>>& Map::GetCities() {
+    return Cities;
+}
+std::vector<int>& Map::GetSeq() {
+    return Seq;
 }
 
-size_t Map::GetWidth() const { return size; }  //возвращает количество столбцов
-size_t Map::GetHeight() const { return size; } // возвращает количество строк
-
-std::vector<std::pair<double, double>> &Map::GetCities() { return Cities; }
-std::vector<int> &Map::GetSeq() { return Seq; }
-
-void SwapSubsequence(int i, int j, std::vector<int> &arr)
-{
+void SwapSubsequence(int i, int j, std::vector<int>& arr) {
     double a;
-    if ((j - i - 1) % 2 != 0)
-    { //если нечетное количество эл-тов между ними
+    if ((j - i - 1) % 2 != 0) {  //если нечетное количество эл-тов между ними
         int z = i + (j - i - 2) / 2 + 1;
-        for (int k = i; k <= j / 2; ++k)
-        {
-            a = arr[k];
-            arr[k] = arr[k + 2 * (z - k)];
+        for (int k = i; k <= j / 2; ++k) {
+            a                    = arr[k];
+            arr[k]               = arr[k + 2 * (z - k)];
             arr[k + 2 * (z - k)] = a;
         }
-    }
-    else
-    {
+    } else {
         int z = i + (j - i - 1) / 2;
-        for (int k = i; k <= j / 2; ++k)
-        {
-            a = arr[k];
-            arr[k] = arr[k + 2 * (z - k) + 1];
+        for (int k = i; k <= j / 2; ++k) {
+            a                        = arr[k];
+            arr[k]                   = arr[k + 2 * (z - k) + 1];
             arr[k + 2 * (z - k) + 1] = a;
         }
     }
 }
 
-void Map::InitializeCities()
-{
-    for (int i = 0; i < this->GetHeight(); ++i)
-    {
+void Map::InitializeCities() {
+    for (int i = 0; i < this->GetHeight(); ++i) {
         //{GetCities()[i] = {rand()%10, rand()%10};}
-        {
-            GetCities()[i] = {random(0, 100.), random(0, 100.)};
-        }
+        { GetCities()[i] = {random(0, 100.), random(0, 100.)}; }
     }
 }
 
-double Map::CalculateEnergy(std::vector<int> stateCandidate)
-{
-    int n = stateCandidate.size();
+double Map::CalculateEnergy(std::vector<int> stateCandidate) {
+    int    n = stateCandidate.size();
     double E = 0;
-    for (int i = 1; i < n - 1; ++i)
-    {
-        E = E + sqrt((GetCities()[i + 1].first - GetCities()[i].first) * (GetCities()[i + 1].first - GetCities()[i].first) + (GetCities()[i + 1].second - GetCities()[i].second) * (GetCities()[i + 1].second - GetCities()[i].second));
+    for (int i = 1; i < n - 1; ++i) {
+        E = E + sqrt((GetCities()[i + 1].first - GetCities()[i].first) *
+                         (GetCities()[i + 1].first - GetCities()[i].first) +
+                     (GetCities()[i + 1].second - GetCities()[i].second) *
+                         (GetCities()[i + 1].second - GetCities()[i].second));
     }
-    E = E + sqrt((GetCities()[n - 1].first - GetCities()[n - 2].first) * (GetCities()[n - 1].first - GetCities()[n - 2].first) + (GetCities()[n - 1].second - GetCities()[n - 2].second) * (GetCities()[n - 1].second - GetCities()[n - 2].second));
+    E = E + sqrt((GetCities()[n - 1].first - GetCities()[n - 2].first) *
+                     (GetCities()[n - 1].first - GetCities()[n - 2].first) +
+                 (GetCities()[n - 1].second - GetCities()[n - 2].second) *
+                     (GetCities()[n - 1].second - GetCities()[n - 2].second));
     return E;
 }
 
-void Map::GenerateStateCandidate(std::vector<int> &)
-{
-    //    seq - предыдущее состояние (маршрут), из которого мы хотим получить состояние-кандидат
-    int n = GetWidth(); // определяем размер последовательности
-    int i = rand() % n; //генерируем целое случайное число
-    int j = rand() % n; // генерируем целое случайное число
+void Map::GenerateStateCandidate(std::vector<int>&) {
+    //    seq - предыдущее состояние (маршрут), из которого мы хотим получить
+    //    состояние-кандидат
+    int n = GetWidth();  // определяем размер последовательности
+    int i = rand() % n;  //генерируем целое случайное число
+    int j = rand() % n;  // генерируем целое случайное число
 
     if (i > j)
-        SwapSubsequence(j, i, GetSeq()); // обращаем подпоследовательность
+        SwapSubsequence(j, i, GetSeq());  // обращаем подпоследовательность
     else
-        SwapSubsequence(i, j, GetSeq()); // обращаем подпоследовательность
-    //return GetSeq();
+        SwapSubsequence(i, j, GetSeq());  // обращаем подпоследовательность
+    // return GetSeq();
 }
 
-double DecreaseTemperature(double initialTemperature, int i)
-{
-    //initialTemperature - начальная температура
-    //i - номер итерации
+double DecreaseTemperature(double initialTemperature, int i) {
+    // initialTemperature - начальная температура
+    // i - номер итерации
     return initialTemperature * 0.1 / i;
 }
 
-double GetTransitionProbability(double dE, double T)
-{
+double GetTransitionProbability(double dE, double T) {
     return exp(-dE / T);
 }
 
-double MakeTransit(double probability)
-{
+double MakeTransit(double probability) {
     double value = std::rand() % 1;
     if (value <= probability)
         return 1;
@@ -95,59 +91,57 @@ double MakeTransit(double probability)
         return 0;
 }
 
-double random(double min, double max)
-{
+double random(double min, double max) {
     return (double)(rand()) / RAND_MAX * (max - min) + min;
 }
 
-std::vector<int> SimulatedAnnealing(Map map, double initialTemperature, double endTemperature)
-{
+std::vector<int> SimulatedAnnealing(Map map, double initialTemperature,
+                                    double endTemperature) {
+    int n = map.GetHeight();  // получаем размер вектора городов
+                              //    for (int i = 0; i < n; ++i) {
+                              //        map.GetSeq()[i] = (rand() % 100);
+                              //    }
 
-    int n = map.GetHeight(); // получаем размер вектора городов
-                             //    for (int i = 0; i < n; ++i) {
-                             //        map.GetSeq()[i] = (rand() % 100);
-                             //    }
-
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         map.GetSeq()[i] = i;
     }
 
-    for (int i = 0; i < n; ++i)
-    {
-        //map.GetSeq()[i] = (rand() % 100);
+    for (int i = 0; i < n; ++i) {
+        // map.GetSeq()[i] = (rand() % 100);
         std::random_shuffle(map.GetSeq().begin(), map.GetSeq().end());
     }
-    // Функция randperm(n) - генерирует случайныую последовательность из целых чисел от 1 до n
+    // Функция randperm(n) - генерирует случайныую последовательность из целых
+    // чисел от 1 до n
 
-    double currentEnergy = map.CalculateEnergy(map.GetSeq()); // вычисляем энергию для первого состояния
+    double currentEnergy = map.CalculateEnergy(
+        map.GetSeq());  // вычисляем энергию для первого состояния
     double T = initialTemperature;
 
-    for (int i = 1; i < 1000000; ++i)
-    {
-        map.GenerateStateCandidate(map.GetSeq()); // получаем состояние-кандидат
+    for (int i = 1; i < 1000000; ++i) {
+        map.GenerateStateCandidate(
+            map.GetSeq());  // получаем состояние-кандидат
         std::vector<int> stateCandidate = map.GetSeq();
-        double candidateEnergy = map.CalculateEnergy(stateCandidate); // вычисляем его энергию
+        double           candidateEnergy =
+            map.CalculateEnergy(stateCandidate);  // вычисляем его энергию
 
         double p = GetTransitionProbability(candidateEnergy - currentEnergy, T);
-        if (candidateEnergy < currentEnergy)
-        {                                    // если кандидат обладает меньшей энергией
-            currentEnergy = candidateEnergy; // то оно становится текущим состоянием
+        if (candidateEnergy <
+            currentEnergy) {  // если кандидат обладает меньшей энергией
+            currentEnergy =
+                candidateEnergy;  // то оно становится текущим состоянием
             map.GetSeq() = stateCandidate;
-        }
-        else
-        { // иначе, считаем вероятность
-            if (MakeTransit(p))
-            { // и смотрим, осуществится ли переход
+        } else {  // иначе, считаем вероятность
+            if (MakeTransit(p)) {  // и смотрим, осуществится ли переход
                 currentEnergy = candidateEnergy;
-                map.GetSeq() = stateCandidate;
+                map.GetSeq()  = stateCandidate;
             }
         }
 
-        T = DecreaseTemperature(initialTemperature, i); // уменьшаем температуру
-        if (T <= endTemperature)                        // условие выхода
+        T = DecreaseTemperature(initialTemperature,
+                                i);  // уменьшаем температуру
+        if (T <= endTemperature)     // условие выхода
             return map.GetSeq();
     }
 
-    //return map.GetSeq();
+    return map.GetSeq();
 }
